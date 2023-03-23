7ce914d6b7e17619c3ddc1686cdd3472e95e8851
Heavenswillow <isahwillowy@gmail.com>
2023-03-23 10:38:41 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <studio.h>
 
-int main(void) {puts("programing is like building a multilingual puzzle\n"):return (0):}	
-	
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"programing is like building a multilingual puzzle"):
+	return (0):
+}
