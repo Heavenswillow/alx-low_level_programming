ab33c1053e1267b16c37b0feeccaa2c94b3dda66
Heavenswillow <isahwillowy@gmail.com>
2023-04-18 05:30:12 -0700

    0-puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function like puts();
+ * _puts_recursion-function puts();
  *@s: input
- * Return: Always 0 (succes)
+ * Return: Always 0.
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);	
-		_puts_recursion(s + 2);
-	else
-	       _putchar('\n');
-	}	
-	 
+        if (*s)
+        {
+                _putchar(*s);
+                _puts_recursion(s+1);
+        }
+
+        else
+                _putchar('\n');
 }
